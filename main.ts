6a046c9c82eb96671f894be7e3a898d2e80b56de
Vincent Talldira <vincent@tallvincent.com>
2025-04-24 22:43:47 +0930

    Commands to toggle checkboxes to different icons.

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -10,77 +8,160 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+const toggleCheckbox = (editor: Editor, char: string) => {
+	const cursor = editor.getCursor();
+	const line = editor.getLine(cursor.line);
+	console.log(line)
+
+	// Regex to match a checkbox
+	const checkboxRegex = /- \[([^\]]+)\]/;
+	const match = line.match(checkboxRegex);
+	console.log(match)
+
+	if (match) {
+		const currentState = match[1];
+		const newState = currentState !== char ? char : ' ';
+		const newLine = line.replace(checkboxRegex, `- [${newState}]`);
+
+		editor.setLine(cursor.line, newLine);
+		editor.setCursor(cursor)
+	}
+}
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'toggle-checkbox-incomplete',
+			name: 'Toggle Checkbox Incomplete',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, '/')
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+		this.addCommand({
+			id: 'toggle-checkbox-canceled',
+			name: 'Toggle Checkbox Canceled',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, '-')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-forwarded',
+			name: 'Toggle Checkbox Forwarded',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, '>')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-scheduling',
+			name: 'Toggle Checkbox Scheduling',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, '<')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-question',
+			name: 'Toggle Checkbox Question',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, '?')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-important',
+			name: 'Toggle Checkbox Important',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, '!')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-star',
+			name: 'Toggle Checkbox Star',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, '*')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-quote',
+			name: 'Toggle Checkbox Quote',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, '"')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-location',
+			name: 'Toggle Checkbox Location',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'l')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-bookmark',
+			name: 'Toggle Checkbox Bookmark',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'b')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-information',
+			name: 'Toggle Checkbox Information',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'i')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-savings',
+			name: 'Toggle Checkbox Savings',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'S')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-idea',
+			name: 'Toggle Checkbox Idea',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'I')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-pro',
+			name: 'Toggle Checkbox Pro',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'p')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-con',
+			name: 'Toggle Checkbox Con',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'c')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-fire',
+			name: 'Toggle Checkbox Fire',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'f')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-key',
+			name: 'Toggle Checkbox Key',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'k')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-win',
+			name: 'Toggle Checkbox Win',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'w')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-up',
+			name: 'Toggle Checkbox Up',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'u')
+		});
+
+		this.addCommand({
+			id: 'toggle-checkbox-down',
+			name: 'Toggle Checkbox Down',
+			editorCallback: (editor: Editor) => toggleCheckbox(editor, 'd')
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() { }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -88,22 +169,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -116,7 +181,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
